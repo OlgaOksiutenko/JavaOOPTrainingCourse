25b816f942f04418e2ad66662b557c1234fb2e62
alex <alexander.tyshchenko@gmail.com>
2015-05-27 22:28:41 +0300

    improved ThreadJoin example

--- src/main/java/com/tyshchenko/training/java/oop/lesson6/ThreadJoinExample.java
+++ src/main/java/com/tyshchenko/training/java/oop/lesson6/ThreadJoinExample.java
@@ -6,24 +6,35 @@ package com.tyshchenko.training.java.oop.lesson6;
 public class ThreadJoinExample {
 
     public static void main(String[] args) {
-        MyCounter counter = new MyCounter();
-        counter.start();
+        MyCounter counter1 = new MyCounter("counter_1");
+        MyCounter counter2 = new MyCounter("counter_2");
+        counter1.start();
+        counter2.start();
 
         try {
-            counter.join();
+            counter1.join();
+            counter2.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+        System.out.println("End Main");
     }
 
     private static final class MyCounter extends Thread {
 
+        private String name;
+
+        public MyCounter(String name) {
+            this.name = name;
+        }
+
         @Override
         public void run() {
             int x = 1;
-            while (x < 1000) {
-                System.out.println(x);
+            while (x < 10) {
                 x++;
+                System.out.println(name + " -> " + x);
             }
             System.out.println("MyCounter thread finished");
         }
